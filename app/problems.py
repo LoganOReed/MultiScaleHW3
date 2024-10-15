c41ba600e91e1236e74776a78e09f32416a95f2d
LoganOReed <me@loganreed.org>
2024-10-14 20:59:37 -0400

    Added N=6 case

--- app/problems.py
+++ app/problems.py
@@ -10,12 +10,26 @@ from matplotlib.colors import ListedColormap
 def all():
     one()
     two()
+    six()
 
 def update(x, rule, n):
     next = [0]*n
     for i in range(n):
         next[i] = rule[(x[(i - 1) % n],x[i],x[(i + 1) % n])]
     return next
+
+def avg_in_degree(g):
+    """docstring for avg_in_degree"""
+    print(g)
+    avg_in_degree = sum(j for i,j in g.in_degree())
+    print(f"with self-loops: {avg_in_degree / len(g.nodes)}")
+    # Remove self loops
+    g_temp = g
+    for v in nx.selfloop_edges(g):
+        g_temp.remove_edge(v[0], v[1])
+    avg_in_degree = sum(j for i,j in g_temp.in_degree())
+    print(f"without self-loops: {avg_in_degree / len(g.nodes)}")
+
  
 def one():
     """4 Cell Lattice with periodic bdy conditions"""
@@ -36,10 +50,13 @@ def one():
         x = [1 if v & 2**i > 0 else 0 for i in range(N - 1, -1, -1)]
         # end node is created by converting state to int
         g.add_edge(v, int(''.join(map(str, update(x, rule, N))), 2))
-    print(g)
+    print(f"N={N}")
     connected_components = [c for c in nx.connected_components(g.to_undirected())]
     w = math.ceil(math.sqrt(len(connected_components)))
     h = math.ceil(len(connected_components) / w)
+
+    # avg in degree
+    avg_in_degree(g)
 
     plt.figure(1, figsize = (12,12))
     # plt.figure(1, figsize = (20,20))
@@ -83,6 +100,9 @@ def two():
     w = math.ceil(math.sqrt(len(connected_components)))
     h = math.ceil(len(connected_components) / w)
 
+    # avg in degree
+    avg_in_degree(g)
+
     plt.figure(1, figsize = (12,12))
     # plt.figure(1, figsize = (20,20))
     for i in range(len(connected_components)):
@@ -98,4 +118,48 @@ def two():
     with open('output/two/adj.tex', 'w') as f:
         print(tabulate(adj, headers, tablefmt="latex"), file=f)
     np.savetxt('output/two/adj.csv', adj, delimiter=',', header=','.join([f'{x:05b}' for x in range(2**N)]), fmt='%i')
+    plt.close()
+
+def six():
+    """6 Cell Lattice with periodic bdy conditions"""
+    N = 6
+    rule = {
+            (1,1,1): 0,
+            (1,1,0): 1,
+            (1,0,1): 1,
+            (1,0,0): 0,
+            (0,1,1): 1,
+            (0,1,0): 1,
+            (0,0,1): 1,
+            (0,0,0): 0
+            }
+    g = nx.DiGraph()
+    for v in range(2**N):
+        # Creates lattice elements from int v
+        x = [1 if v & 2**i > 0 else 0 for i in range(N - 1, -1, -1)]
+        # end node is created by converting state to int
+        g.add_edge(v, int(''.join(map(str, update(x, rule, N))), 2))
+    print(g)
+    connected_components = [c for c in nx.connected_components(g.to_undirected())]
+    w = math.ceil(math.sqrt(len(connected_components)))
+    h = math.ceil(len(connected_components) / w)
+
+    # avg in degree
+    avg_in_degree(g)
+
+    plt.figure(1, figsize = (12,12))
+    # plt.figure(1, figsize = (20,20))
+    for i in range(len(connected_components)):
+        plt.subplot(h, w, i + 1)
+        nx.draw(nx.subgraph(g, connected_components[i]), with_labels = True)
+
+    # plt.show()
+    # print(nx.adjacency_matrix(g))
+    adj = nx.to_numpy_array(g)
+    plt.savefig(f'output/six/graph.png')
+    # NOTE: Need to change 04b when updating N
+    headers = [f'{x:06b}' for x in range(2**N)]
+    with open('output/six/adj.tex', 'w') as f:
+        print(tabulate(adj, headers, tablefmt="latex"), file=f)
+    np.savetxt('output/six/adj.csv', adj, delimiter=',', header=','.join([f'{x:06b}' for x in range(2**N)]), fmt='%i')
     plt.close()
